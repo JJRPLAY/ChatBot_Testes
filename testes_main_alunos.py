eb70f1107591f6bd71c6b1ebbbc5116b49ce36b7
JJRPLAY <agostinhojuniorjive@gmail.com>
2025-07-27 21:40:10 +0100

    Teste cozinha & teste de historia
    Ok!

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -25,9 +25,11 @@ class TestObterResposta(unittest.TestCase):
 
     def teste_historia_portugal(self):
         """Teste de respostas sobre história de Portugal - 1 teste"""
+        self.assertEqual(obter_resposta("historia de portugal"), "Portugal tem uma rica história...")
 
     def teste_cozinhar(self):
         """Teste de respostas sobre cozinhar - 1 teste"""
+        self.assertEqual(obter_resposta("cozinhar"), "Cozinhar é uma arte que envolve a preparação de alimentos...")
 
     def teste_programar(self):
         """Teste de respostas sobre programar - 2 testes"""
